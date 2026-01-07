28d16c156e1e366f4d2ed6da8705a97e885cd2d4
yashureddyB <yaswanthreddybhumireddy3@gmail.com>
2026-01-07 10:30:44 +0530

    initial setup

---
+++ src/main/java/com/yashu/auth/entity/AuthEntity.java
@@ -0,0 +1,26 @@
+package com.yashu.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "AUTH_TL")
+@Data
+public class AuthEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@Column(unique = true)
+	private String email;
+
+	private String password;
+
+}
